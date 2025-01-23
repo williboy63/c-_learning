fc9932dd2dd92f83d43a20d5e655d3216236a2d1
williboy63 <william.stours@hotmail.com>
2025-01-23 09:30:59 -0500

    How many guesses added to game 23/01/2025 @ 0930

--- game/game.cpp
+++ game/game.cpp
@@ -3,17 +3,39 @@
 #include <cstdlib>
 #include <ctime>
 
+
+void print_array(int array[], int count)
+{
+    for (int i = 0 ; i < count; i++)
+    {
+        std::cout << array[i] << "\t";
+        
+    }
+    
+    std::cout << "\n";
+}
+
+
+
 void play_game()
 {
+    int guesses[250];
+    int guess_count = 0;
+
     int random = rand() % 251; //Generate a random number from 0 to 250
     std::cout << "Guess the number: ";
     while(true)
     {
+
         int guess;
         std::cin >> guess;
+
+        guesses[guess_count++] = guess;
+
         if (guess == random)
         {
             std::cout << "Congrats!" << std::endl;
+            std::cout << "Took you " << guess_count << " guesses to find the number" << std::endl;
             break;
         } else if(guess < random)
         {
@@ -23,6 +45,8 @@ void play_game()
             std::cout << "Too high\n";
         }
     }
+    print_array(guesses, guess_count);
+
 }
 
 
